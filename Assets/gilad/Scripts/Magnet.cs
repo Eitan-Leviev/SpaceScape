77b2bcd649e6bf8b991f54c0cb0049009fdfcfb3
Gilad Binstok <giladbin@cs.huji.ac.il>
2022-05-07 23:28:52 +0300

    vent and magnet

---
+++ Assets/gilad/Scripts/Magnet.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magnet : MonoBehaviour
+{
+    [SerializeField] private float speed = 1f;
+    // Start is called before the first frame update
+
+    private Transform _ball = null;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        print("trigger enter");
+        _ball = other.gameObject.transform;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        print("trigger exit");
+        _ball = null;
+    }
+
+    private void FixedUpdate()
+    {
+        if (_ball != null)
+        {
+            var normal = (_ball.position - transform.position).normalized * speed;
+            _ball.position += normal;
+
+        }
+    }
+}
