5e45c5b51e60c1dcb89df4df6ee1d0907fd175da
eitan lev <eitanleviev111@gmail.com>
2022-05-27 14:38:30 +0300

    limits bug fixed

--- Assets/eitan/scripts/Limits.cs
+++ Assets/eitan/scripts/Limits.cs
@@ -5,7 +5,16 @@ using UnityEngine;
 
 public class Limits : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D other)
+    // private void OnCollisionEnter2D(Collision2D other)
+    // {
+    //     if (other.gameObject.name == "ball")
+    //     {
+    //         GameObject.Find("ball").GetComponent<BallEitan>().ResetPlayer();
+    //         // print("CCC");
+    //     }
+    // }
+
+    private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.name == "ball")
         {
