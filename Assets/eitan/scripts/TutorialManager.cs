36861fb122b25b031870029c886fe331a27dc595
eitan lev <eitanleviev111@gmail.com>
2022-06-05 21:06:40 +0300

    scroll and fire tutor - in process
    remains to detect players actions

--- Assets/eitan/scripts/TutorialManager.cs
+++ Assets/eitan/scripts/TutorialManager.cs
@@ -1,28 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
+    private GameObject canvas;
+    
     public static bool LeftClickTutorialDoneFlag = false;
     
-    // Start is called before the first frame update
-    void Start()
+    public static bool rotateCannonTutorialDoneFlag = false;
+
+    private void Awake()
     {
-        
+        canvas = GameObject.Find("Canvas");
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if (Input.GetKeyDown(KeyCode.A))
-        // {
-        //     LeftClickTutorialEnding();
-        // }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            rotateCannonTutorialEnding();
+        }
     }
 
     public void LeftClickTutorialEnding()
     {
         GetComponent<Animator>().SetBool("done", false);
+    }
+    
+    public void rotateCannonTutorialEnding()
+    {
+        // scroll disappear 
+        GameObject.Find("tutor scroll canon").GetComponent<Animator>().SetTrigger("fade out");
+        // R click appear 
+        GameObject.Find("tutor R click").GetComponent<Animator>().SetTrigger("fade in");
     }
 }
