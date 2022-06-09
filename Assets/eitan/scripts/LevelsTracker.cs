48ff7a5b563d9c66560482041eb513f365f63eed
eitan lev <eitanleviev111@gmail.com>
2022-06-10 02:28:02 +0300

    levels tracker added  ?

---
+++ Assets/eitan/scripts/LevelsTracker.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using gilad.Scripts;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelsTracker : MonoBehaviour
+{
+    private void Awake()
+    {
+        GetComponent<Text>().text = $"Level {GameManager.Level} / 17";
+    }
+}
