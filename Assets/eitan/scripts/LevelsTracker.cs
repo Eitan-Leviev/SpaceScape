69d61b192726a23d655f7d82ac61bde8184c55b3
eitan lev <eitanleviev111@gmail.com>
2022-06-10 18:55:57 +0300

    added part txt + a bit UI

--- Assets/eitan/scripts/LevelsTracker.cs
+++ Assets/eitan/scripts/LevelsTracker.cs
@@ -9,6 +9,6 @@ public class LevelsTracker : MonoBehaviour
 {
     private void Awake()
     {
-        GetComponent<Text>().text = $"Level {GameManager.Level} / 17";
+        GetComponent<Text>().text = $"Level {GameManager.Level} | 17";
     }
 }
